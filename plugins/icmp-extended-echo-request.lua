ce859c898956dc70c446e60346c4130bc31d50fb
teddy <teddy@sfc.wide.ad.jp>
2018-05-27 19:32:50 +0900

    initial commit

---
+++ plugins/icmp-extended-echo-request.lua
@@ -0,0 +1,37 @@
+-- 
+--    Wireshark Plugin for ICMP Extended Echo Request Message
+--
+--    0                   1                   2                   3
+--    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+--    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+--    |     Type      |     Code      |          Checksum             |
+--    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+--    |           Identifier          |Sequence Number|   Reserved  |L|
+--    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+-- 
+
+local icmp_ext_echo_req_proto = Proto("ICMP_Extended_Echo_Request", "ICMP Extended Echo Request")
+local f = icmp_ext_echo_req_proto.fields
+local icmpv6_type = Field.new("icmpv6.type")
+
+f.type = ProtoField.new("Type", "icmp.ext.echo.req.type", ftypes.UINT8, nil, base.DEC)
+f.code = ProtoField.new("Code", "icmp.ext.echo.req.code", ftypes.UINT8, nil, base.DEC)
+f.checksum = ProtoField.new("Checksum", "icmp.ext.echo.req.checksum", ftypes.UINT8, nil, base.HEX)
+f.identifier = ProtoField.new("Identifier", "icmp.ext.echo.req.identifier", ftypes.UINT8, nil, base.HEX)
+f.sequence = ProtoField.new("Sequence Number", "icmp.ext.echo.req.seq", ftypes.UINT8, nil, base.DEC)
+f.reserved = ProtoField.new("Reserved", "icmp.ext.echo.req.rsrvd", ftypes.UINT8, nil, base.DEC)
+
+function icmp_ext_echo_req_proto.dissector(buffer, pinfo, tree)
+	if icmpv6_type().value == 160 then
+		local subtree = tree:add(icmp_ext_echo_req_proto, buffer(54, 8))
+		subtree:add(f.type, buffer(54, 1))
+		subtree:add(f.code, buffer(55, 1))
+		subtree:add(f.checksum, buffer(56, 2))
+		subtree:add(f.identifier, buffer(58, 2))
+		subtree:add(f.sequence, buffer(60, 1))
+		subtree:add(f.reserved, buffer(61, 1))
+	end
+end
+
+register_postdissector(icmp_ext_echo_req_proto)
+
